8d38d8d2f9418d0f664a29c0a493e0699792f2c2
Jonathan Fung <jonathanfung2000@gmail.com>
2024-12-09 23:26:54 -0800

    home: add mpv bindings

--- home-jonat.nix
+++ home-jonat.nix
@@ -643,6 +643,13 @@ rec {
     };
   };
 
+  programs.mpv = {
+    enable = true;
+    bindings = {
+      r = "cycle_values video-rotate 90 180 270 0";
+    };
+  };
+
   programs.fuzzel = {
     enable = true;
     settings = {
